d535890464d78ee68b81b6b11e892db603008eac
eagleduk <eagleduk@gmail.com>
2020-04-16 15:22:47 +0900

    Updates

---
+++ precache-manifest.079c26fb8bfd669ced3f148c116a451b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ed5f96a92fbc7501d1760277193d692",
+    "url": "/TIC_TAC_TOE/index.html"
+  },
+  {
+    "revision": "1e7c7133307e46f8d6f0",
+    "url": "/TIC_TAC_TOE/static/css/main.6dd98c10.chunk.css"
+  },
+  {
+    "revision": "ee43a992b6306c4cbf2b",
+    "url": "/TIC_TAC_TOE/static/js/2.8fde8be6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TIC_TAC_TOE/static/js/2.8fde8be6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e7c7133307e46f8d6f0",
+    "url": "/TIC_TAC_TOE/static/js/main.a7db8120.chunk.js"
+  },
+  {
+    "revision": "fdc56bcb53856279be86",
+    "url": "/TIC_TAC_TOE/static/js/runtime-main.5b09f102.js"
+  }
+]);
